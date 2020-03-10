b3388303cb196442d3a701b94aa970eae72bf36e
Mikhail Shilkov <github@mikhail.io>
2020-03-10 09:59:38 +0100

    Infinicache

--- lab/arm/materials/app/hello/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context) {
-  return {
-    status: 200,
-    body: "You've successfully deployed a Function App!",
-    headers: {
-      "content-type": "text/html",
-    },
-  };
-};
